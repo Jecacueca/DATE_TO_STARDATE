25fcff85e608f7a52c8e9d3e2dc2f5f37c0f5ff0
Jess Silva Carvalho <jc.carvalho@outlook.com>
2024-06-10 11:51:54 +0100

    implemented stardate ccalculation logic in the pages controller. As well as display. Need to use JS to handle stopping page reaload

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,15 @@
+require 'date'
+
 class PagesController < ApplicationController
   def home
+    if params[:date]
+      calculate_stardate
+    end
+  end
+
+  def calculate_stardate
+    @date = params[:date].to_date
+    @stardate = "#{@date.year - 1900}#{'%02i' % @date.month}.#{'%02i' % @date.day}"
+    redirect_to root_path
   end
 end
