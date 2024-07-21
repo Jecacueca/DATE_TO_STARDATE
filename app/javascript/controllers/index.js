11c1e41e21f8dc0013518a518bf36f0590eafe07
Jess Silva Carvalho <jc.carvalho@outlook.com>
2024-07-21 22:43:37 +0100

    Implemented Stimulus animated number for date display

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -9,3 +9,7 @@ eagerLoadControllersFrom("controllers", application)
 // Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
 // import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
 // lazyLoadControllersFrom("controllers", application)
+
+// Animated number
+import AnimatedNumber from '@stimulus-components/animated-number'
+application.register('animated-number', AnimatedNumber)
